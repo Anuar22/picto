d8626bb5fb97942fe0230d738a864e04285494ff
utsaho <pipul.boddyo@gmail.com>
2025-04-17 17:02:17 +0600

    NavBar structured

--- src/components/common/navbar/NavBar.jsx
+++ src/components/common/navbar/NavBar.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import logo from "../../../assets/logo.jpg";
 const NavBar = () => {
+  //   Menu list for the navbar
+  const menu = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+      <li>
+        <a>Process</a>
+      </li>
+      <li>
+        <a>Portfolio</a>
+      </li>
+      <li>
+        <a>Blog</a>
+      </li>
+      <li>
+        <a>Services</a>
+      </li>
+    </>
+  );
+
   return (
-    <div>
-      <div className="navbar  shadow-sm">
-        <div className="navbar-start">
+    <div className="w-full">
+      <div className="navbar shadow-sm flex justify-between px-4 lg:px-20 py-2">
+        <div className="flex">
           <div className="dropdown">
             {/* TODO: Menu for small screen start */}
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden "
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -15,72 +43,32 @@ const NavBar = () => {
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
-                {" "}
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
+                />
               </svg>
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content rounded-box z-1 mt-3 w-52 p-2 shadow font-semibold"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menu}
             </ul>
             {/* Menu for small screen end */}
           </div>
-          {/*Logo section */}
-          <div className="flex btn bg-white text-black btn-ghost border-0">
-            <img src={logo} className="h-8 rounded-2xl" alt="logo" />
-            <p className="text-xl">Brooklyn</p>
+
+          <div /* Logo section */ className="flex bg-whit border-0 gap-1">
+            <img src={logo} className="h-14 rounded-2xl" alt="logo" />
+            <p className="text-4xl font-semibold my-auto">Brooklyn</p>
           </div>
-          {/*Logo section end */}
         </div>
 
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+        <div className=" hidden lg:flex">
+          <ul className="menu menu-horizontal text-xl px-1 gap-3">{menu}</ul>
+          <a className="btn btn-lg btn-primary">Contact</a>
         </div>
       </div>
     </div>
