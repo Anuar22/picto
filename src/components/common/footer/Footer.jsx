0216c9c8f2915f3eae9428f518f395e4b03a9e34
utsaho <pipul.boddyo@gmail.com>
2025-04-27 16:59:30 +0600

    footer file organized

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import logo from "../../../assets/logo.png";
 
 const Footer = () => {
+  const copyrightYear = new Date().getFullYear();
+
+  /* Footer nabLinks */
   const navItems = [
     { id: 1, name: "Home", url: "Home" },
     { id: 2, name: "About", url: "About" },
@@ -9,26 +12,30 @@ const Footer = () => {
     { id: 4, name: "Portfolio", url: "Portfolio" },
     { id: 5, name: "Blog", url: "Blog" },
     { id: 6, name: "Services", url: "Services" },
+    { id: 7, name: "Contact", url: "Contact" },
   ];
 
   return (
-    <div>
-      <div className="bg-gray-800 p-4 mt-4">
-        <div className="flex bg-whit border-0 gap-1 lg:pe-10">
+    <div className="bg-[#2A374A] p-4 mt-4 h-[320px] flex justify-between">
+      <div className="flex justify-between mx-0 lg:px-[15%] items-center h-full w-full text-neutral-200 lg:bg-red-700 md:bg-amber-100">
+        <div className="flex bg-whit border-0 gap-1 lg:me-10 shrink-0">
           <img src={logo} className="h-14 rounded-2xl" alt="logo" />
-          <p className="text-4xl font-semibold my-auto text-white">Brooklyn</p>
+          <p className="text-4xl font-semibold my-auto">Brooklyn</p>
         </div>
-      </div>
-      <div>
-        {navItems.map((item) => (
-          <a
-            key={item.id}
-            className="text-black hover:text-purple-700 mx-2"
-            href={`#${item.url.toLowerCase()}`}
-          >
-            {item.name}
-          </a>
-        ))}
+        <div className="mx-3 flex">
+          {navItems.map((item) => (
+            <a
+              key={item.id}
+              className=" hover:text-purple-700 mx-4"
+              href={`#${item.url.toLowerCase()}`}
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className="shrink-0">
+          <p className="">Copyright &copy; {copyrightYear} Picto.</p>
+        </div>
       </div>
     </div>
   );
