a5b4d8f8a3109c697e40af9b54a422a9400bf7b1
utsaho <pipul.boddyo@gmail.com>
2025-07-29 14:10:28 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-const repoName = "picto"; // Replace with your repo name
+const repoName = "picto";
 
 // https://vite.dev/config/
 export default defineConfig({
