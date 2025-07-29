af06ff94e9cdffd92792da30e1819fe2f6333eb3
utsaho <pipul.boddyo@gmail.com>
2025-07-29 15:09:42 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-const repoName = "picto";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: `/${repoName}/`,
-  plugins: [react(), tailwindcss()],
-  //   COMMENT: server object is used for testing responsive design on different devices
-  //   server: {
-  //     open: true,
-  //     host: false, // Allow access from network devices
-  //     proxy: {
-  //       "/api": {
-  //         target: "http://localhost:5000",
-  //         changeOrigin: true,
-  //         secure: true,
-  //       },
-  //     },
-  //   },
+export default defineConfig(({ mode }) => {
+  // Load env file for the current mode in the root directory.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: `/${env.VITE_REPO_NAME}/`,
+    plugins: [react(), tailwindcss()],
+    //   COMMENT: server object is used for testing responsive design on different devices
+    //   server: {
+    //     open: true,
+    //     host: false, // Allow access from network devices
+    //     proxy: {
+    //       "/api": {
+    //         target: "http://localhost:5000",
+    //         changeOrigin: true,
+    //         secure: true,
+    //       },
+    //     },
+    //   },
+  };
 });
